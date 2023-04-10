758371f32b42c6d94fbc8f2d16a3bf41f84e14d9
Hashir-a <hashir.a@outlook.com>
2023-04-10 22:39:55 +0500

    added login page

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,15 +1,61 @@
-import React from 'react'
+import React, { Fragment, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
-  return (
-    <div className='container'>
-      <h2>Login</h2><br/>
-      <h2>Login</h2><br/>
-      <h2>Login</h2><br/>
-      <h2>Login</h2><br/>
-      <h2>Login</h2><br/>
-    </div>
-  )
-}
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
 
-export default Login
+  const { email, password } = formData;
+  const onChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = async (e) => {
+    console.log('I am here');
+    e.preventDefault();
+    
+      console.log('Success');
+    
+  };
+
+  return (
+    <Fragment>
+      <div className='container'>
+        <h1 className='large text-primary'>Sign In</h1>
+        <p className='lead'>
+          <i className='fas fa-user'></i> Sign in to your account
+        </p>
+        <form className='form' onSubmit={onSubmit}>
+          <div className='form-group'>
+            <input
+              type='email'
+              placeholder='Email Address'
+              name='email'
+              value={email}
+              onChange={(e) => onChange(e)}
+              required
+            />
+          </div>
+          <div className='form-group'>
+            <input
+              type='password'
+              placeholder='Password'
+              name='password'
+              minLength='6'
+              value={password}
+              onChange={(e) => onChange(e)}
+              required
+            />
+          </div>
+          <input type='submit' className='btn btn-primary' value='Login' />
+        </form>
+        <p className='my-1'>
+          Don't have an account? <Link to='/Register'>Sign Up</Link>
+        </p>
+      </div>
+    </Fragment>
+  );
+};
+
+export default Login;
